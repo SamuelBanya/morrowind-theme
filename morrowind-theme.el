5253d03e4aba8fe4f9d30c1483c2189616ce8990
Samuel Banya <sbanya@fastmail.com>
2023-03-11 13:19:08 -0600

    Adding comment line change to conform to MELPA

--- morrowind-theme.el
+++ morrowind-theme.el
@@ -24,8 +24,9 @@
 ;; This file is not part of Emacs.
 
 ;;; Commentary:
-;;; An Emacs theme based upon the colors found in the in-game menu of the Elder Scrolls III: Morrowind game by Bethesda
-;;; Nerevar approved... though Dagoth Ur was not present for a related comment upon press release
+;;; An Emacs theme based upon the colors found in the in-game menu of the Elder
+;;; Scrolls III: Morrowind game by Bethesda Nerevar approved... though Dagoth
+;;; Ur was not present for a related comment upon press release
 
 ;;; Code:
 
